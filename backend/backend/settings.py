9b917610fc3e7334915a0a30dc41f27b965fd823
JeffRebiro <jeffrebiro@gmail.com>
2025-07-31 13:11:53 +0300

    Settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -63,14 +63,14 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
-                'django.template.context_processors.messages', # This is correctly enabled
-                'django.contrib.messages.context_processors.messages',
+                'django.contrib.messages.context_processors.messages',  # ✅ correct
             ],
         },
     },
