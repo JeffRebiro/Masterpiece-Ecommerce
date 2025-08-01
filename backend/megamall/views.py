9e0b7898212c8e98fd88013f5bd62e6aa657c746
JeffRebiro <jeffrebiro@gmail.com>
2025-08-01 10:23:25 +0300

    AuthContext

--- backend/megamall/views.py
+++ backend/megamall/views.py
@@ -37,6 +37,7 @@ from requests.auth import HTTPBasicAuth
 from django.views.decorators.csrf import csrf_exempt
 
 from rest_framework import viewsets, permissions
+from rest_framework.permissions import AllowAny
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView
